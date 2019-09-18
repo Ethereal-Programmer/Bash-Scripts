0d7713d4721e9ed07b189697d3d19e727d6b81d4
Kevin <kcmott94@gmail.com>
2019-09-18 10:45:10 -0400

    Update brightness.sh
    Added some comments explaining it since I think this one might genuinely be useful to some people.

--- brightness.sh
+++ brightness.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script is to edit the brightness on my Thinkpad T440 since i3wm likes to break my brightness keys.
+# If you use this, I highly recommend you alias it to save yourself some time.
+
 read -p "Enter Desired Brightness Value Between 0 and 851: " BRIGHTNESS_VALUE
 
 echo ${BRIGHTNESS_VALUE} > /sys/class/backlight/intel_backlight/brightness
